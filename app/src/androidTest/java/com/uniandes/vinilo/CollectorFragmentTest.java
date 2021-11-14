aecc22a341e010df126b73223706213d595a6cc4
rmatiz84 <r.matiz@uniandes.edu.co>
2021-11-14 11:51:52 -0500

    Create CollectorFragmentTest.java

---
+++ app/src/androidTest/java/com/uniandes/vinilo/CollectorFragmentTest.java
@@ -0,0 +1,119 @@
+package com.uniandes.vinilo;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.internal.util.Checks.checkNotNull;
+import static org.hamcrest.Matchers.allOf;
+
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.BoundedMatcher;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.uniandes.vinilo.ui.MainActivity;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class CollectorFragmentTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void navegarColeccionistas(){
+        onView(withId(R.id.navigation_collector)).perform(click());
+    }
+
+
+    @Test
+    public void testListaVisible() {
+        onView(withId(R.id.fragments_rv)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSelectItem_isDetailVisible() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.fragments_rv)).perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testCountItems() {
+        Matcher<View> m = allOf(withId(R.id.fragments_rv),
+                isDisplayed());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        int n = getRecyclerViewCount(m);
+        Assert.assertEquals(true, n > 0);
+        return;
+    }
+
+    public static int getRecyclerViewCount(Matcher matcher) {
+        final int[] num = new int[1];
+        onView(allOf(matcher, isEnabled())).check(matches(new TypeSafeMatcher<View>() {
+            RecyclerView recyclerView = null;
+            @Override
+            public boolean matchesSafely(View view) {
+                recyclerView = (RecyclerView) view;
+                RecyclerView.Adapter adapter = recyclerView.getAdapter();
+                Log.d("Booking Recycler Cnt", adapter.getItemCount() + "");
+                num[0] = adapter.getItemCount();
+                return true;
+            }
+            @Override
+            public void describeTo(Description description) {
+            }
+        }));
+        return num[0];
+    }
+
+    public static Matcher<View> atPosition(final int position, @NonNull final Matcher<View> itemMatcher) {
+        checkNotNull(itemMatcher);
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+            @Override public void describeTo(Description description) {
+                description.appendText("has item at position " + position + ": ");
+                itemMatcher.describeTo(description);
+            }
+            @Override protected boolean matchesSafely(final RecyclerView view) {
+                RecyclerView.ViewHolder viewHolder = view.findViewHolderForAdapterPosition(position);
+                if (viewHolder == null) {
+                    // has no item on such position
+                    return false;
+                }
+                return itemMatcher.matches(viewHolder.itemView);
+            }
+        };
+    }
+
+}
