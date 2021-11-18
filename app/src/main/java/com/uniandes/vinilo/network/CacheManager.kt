b1889946366d57c21a7b27501827c0a357ff3ac3
rmatiz84 <r.matiz@uniandes.edu.co>
2021-11-18 10:58:03 -0500

    Cache en albumes y coleccionistas

--- app/src/main/java/com/uniandes/vinilo/network/CacheManager.kt
+++ app/src/main/java/com/uniandes/vinilo/network/CacheManager.kt
@@ -1,6 +1,7 @@
 package com.uniandes.vinilo.network
 
 import android.content.Context
+import android.content.SharedPreferences
 import com.uniandes.vinilo.models.Comment
 
 class CacheManager(context: Context) {
@@ -12,8 +13,15 @@ class CacheManager(context: Context) {
                     instance = it
                 }
             }
+        const val APP_SPREFS = "com.uniandes.vinilo.app"
+        const val ALBUMS_SPREFS = "com.uniandes.vinilo.albums"
+        fun getPrefs(context: Context, name:String): SharedPreferences {
+            return context.getSharedPreferences(name,
+                Context.MODE_PRIVATE
+            )
+        }
     }
-    private var comments: HashMap<Int, List<Comment>> = hashMapOf()
+    /*private var comments: HashMap<Int, List<Comment>> = hashMapOf()
     fun addComments(albumId: Int, comment: List<Comment>){
         if (!comments.containsKey(albumId)){
             comments[albumId] = comment
@@ -21,5 +29,6 @@ class CacheManager(context: Context) {
     }
     fun getComments(albumId: Int) : List<Comment>{
         return if (comments.containsKey(albumId)) comments[albumId]!! else listOf<Comment>()
-    }
+    }*/
+
 }
